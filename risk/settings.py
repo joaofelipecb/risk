d660c8755f46ee4766b78d84c173e2da6b8b555d
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-05-28 13:42:00 -0300

    PostgreSQL configuration

--- risk/settings.py
+++ risk/settings.py
@@ -72,11 +72,15 @@ WSGI_APPLICATION = 'risk.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+import risk.SecurityConfig
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': risk.SecurityConfig.database['name'],
+        'USER': risk.SecurityConfig.database['user'],
+        'PASSWORD': risk.SecurityConfig.database['password'],
+        'HOST': risk.SecurityConfig.database['host'],
     }
 }
 
