fd8bbb209088a0bdd335414931e853745b11445a
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-05-28 19:41:19 -0300

    added automated test to probabilities

--- riskApp/tests.py
+++ riskApp/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import AccountReceivable
 
-# Create your tests here.
+class ProbabilityTest(TestCase):
+    def test_sum(self):
+        a1 = AccountReceivable(value=1000,probability=0.80)
+        a2 = AccountReceivable(value=1200,probability=0.65)
+        p = ProbabilityMulticiplicity([a1,a2])
+        p.calculate()
+        self.AssertIs(p.get_values(),[0,1000,1200,2200])
+        
