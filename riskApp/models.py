e24726e63a09743398821d9bfe1f9c4100d736ab
João Felipe Chiarelli Bourscheid <joao.felipe.c.b@gmail.com>
2021-05-28 17:03:41 -0300

    Created the initial view

--- riskApp/models.py
+++ riskApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class accountReceivable(models.Model):
+    value = models.DecimalField(decimal_places=2, max_digits=11)
+    probability = models.FloatField()
+    dueDate = models.DateTimeField()
+
